76441826a618a519c714da382d7140f5b6d5ce01
thao1 <dangthuthao12062001@gmail.com>
2022-04-24 17:00:57 +0700

    javaswing

---
+++ src/hieuthuoctunhan/Hieuthuoctunhan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hieuthuoctunhan;
+
+/**
+ *
+ * @author baam
+ */
+public class Hieuthuoctunhan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/hieuthuoctunhan/javaswing.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hieuthuoctunhan;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author baam
+ */
+public class javaswing {
+    public javaswing(){
+        JFrame jframe = new JFrame(" Hiệu Thuốc Tư Nhân ");
+        jframe.setSize(800,600);
+        jframe.setVisible(true);
+        jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+        }
+    public static void main(String[] args){
+        new javaswing();
+        }
+}
+        
